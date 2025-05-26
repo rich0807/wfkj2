23f3bde97ed7e752414a24b4e1944291cb8b5289
rich0807 <3428773947@qq.com>
2025-05-26 11:02:23 +0800

    feat：项目初始化

---
+++ src/main/java/com/rq/rqwfkj/RqWfkjApplication.java
@@ -0,0 +1,14 @@
+package com.rq.rqwfkj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RqWfkjApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RqWfkjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rq/rqwfkj/RqWfkjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rq.rqwfkj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RqWfkjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
